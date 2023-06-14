3ce18ff344ce4bf630a910484c474c46faa84a82
Ikotun Collins <danlogan2003@gmail.com>
2023-06-14 02:16:00 +0100

    x

--- urlShortener/UShort/urls.py
+++ urlShortener/UShort/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
+from . import views 
 
 urlpatterns = [
-    path("short/")
+    path("shortenurl/", views.UrlShortener.as_view())
 ]
 
--- urlShortener/UShort/views.py
+++ urlShortener/UShort/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 import pyshorteners
 
 # Create your views here.
 class UrlShortener(APIView):
     def post(self, request):
         long_url = request.data['url']
-        shortener = pyshorteners.Shortener()
-        shortened_url = shortener.tinyurl.short(url)
-        return shortened_url
-
+        if long_url : 
+            shortener = pyshorteners.Shortener()
+            shortened_url = shortener.tinyurl.short(url)
+            return Response({'short_url' : shortened_url}, status=status.HTTP_200_OK)
+        else :
+            return Response({'error': 'Missing long_url parameter'}, status=status.HTTP_401_UNAUTHORIZED)
+            
 
