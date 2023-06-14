fe503b72f6337aac6ecc1baea60942b6bc08838a
Ikotun Collins <danlogan2003@gmail.com>
2023-06-14 02:10:43 +0100

    url

--- urlShortener/UShort/views.py
+++ urlShortener/UShort/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+import pyshorteners
 
 # Create your views here.
+class UrlShortener(APIView):
+    def post(self, request):
+        long_url = request.data['url']
+        shortener = pyshorteners.Shortener()
+        shortened_url = shortener.tinyurl.short(url)
+        return shortened_url
+
+
